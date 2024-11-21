Fixed the calculation of classification metric.

src/common/logger.py
@@ -23,16 +23,15 @@ class DetectLogger:
         self.results = []
         self.epoch_info = ""
 
-    def add_result(self, result):
-        """
-        auroc, aupr, fpr, accuracy, test_score, valid_loss
-        Args:
-            result:
-
-        Returns:
-
-        """
-        self.results.append(result)
+    def log(self, epoch: int, train_loss: float, auroc: float, aupr: float, fpr: float, accuracy: float, test_score: float, valid_loss: float):
+        self.results.append((auroc, aupr, fpr, accuracy, test_score, valid_loss))
+        info = (
+            f'Epoch: {epoch:02d}, Train Loss: {train_loss:.4f}, Val Loss: {valid_loss:.4f}, '
+            f'AUROC: {100 * auroc:.2f}%, AUPR: {100 * aupr:.2f}%, FPR95: {100 * fpr:.2f}%, Accuracy: {100 * accuracy:.2f}%, '
+            f'Test Score: {100 * test_score:.2f}%'
+        )
+        self.add_epoch_info(info)
+        print(info)
 
     def add_epoch_info(self, info):
         self.epoch_info += info + '\n'
@@ -40,6 +39,7 @@ class DetectLogger:
     def get_statistics(self) -> Metrics:
         result = 100 * torch.tensor(self.results)
         auroc, aupr, fpr, accuracy, test_score, valid_loss = result.T
+        valid_loss = valid_loss / 100
         min_index = valid_loss.argmin().item()
         auroc_val = auroc[min_index].item()
         aupr_val = aupr[min_index].item()
@@ -52,6 +52,11 @@ class DetectLogger:
         print(f'OOD Test Detect FPR95 ↓: {fpr_val:.2f}')
         print(f'OOD Test Detect ACCU  ↑: {acc_val:.2f}')
         print(f'IND Test Accuracy     ↑: {score_val:.2f}')
+        print(
+            f'Epoch: {min_index}, Loss(↓): {valid_loss[min_index]:.4f}, '
+            f'AUROC(↑): {auroc_val:.2f}%, AUPR(↑): {aupr_val:.2f}%, FPR95(↓): {fpr_val:.2f}%, Accuracy(↑): {acc_val:.2f}%, '
+            f'Test Score(↑): {score_val:.2f}%'
+        )
         return Metrics(
             auroc=round(auroc_val, 2),
             aupr=round(aupr_val, 2),

src/main.py
@@ -28,7 +28,7 @@ def setup_args() -> tuple[Arguments, torch.device]:
 
 
 def setup_dataset(args: Arguments, device: torch.device):
-    print("\n==================== Begin Prepare Dataset ====================")
+    print(f"\n{'Begin Prepare Dataset':=^80}")
 
     dataset_ind, dataset_ood_tr, dataset_ood_te = load_dataset(args)
 
@@ -58,7 +58,8 @@ def setup_dataset(args: Arguments, device: torch.device):
     dataset_ood_tr.to(device=device)
     dataset_ood_te.to(device=device)
 
-    print("==================== End Prepare Dataset ====================")
+    print(f"{'End Prepare Dataset':=^80}")
+
     return dataset_ind, dataset_ood_tr, dataset_ood_te, synthesis_ood_dataset, num_classes, num_features
 
 
@@ -104,7 +105,7 @@ def setup_model(args: Arguments, num_features: int, num_classes: int, device: to
 
 
 def train():
-    print(f"\n====================Begin Time: {get_now_datetime()}====================")
+    print(f"\n{'Begin Time: ' + get_now_datetime():=^80}")
     args, device = setup_args()
     dataset_ind, dataset_ood_tr, dataset_ood_te, synthesis_ood_dataset, num_classes, num_features = setup_dataset(args=args, device=device)
     model, criterion, optimizer, logger = setup_model(args=args, num_features=num_features, num_classes=num_classes, device=device)
@@ -115,15 +116,10 @@ def train():
         loss = model.loss_compute(dataset_ind, dataset_ood_tr, synthesis_ood_dataset, criterion, device, args)
         loss.backward()
         optimizer.step()
-        result = evaluate_detect(model, dataset_ind, dataset_ood_te, criterion, eval_acc, args, device)
-        logger.add_result(result)
-        info = (
-            f'Epoch: {epoch:02d}, Loss: {loss:.4f}, AUROC: {100 * result[0]:.2f}%, AUPR: {100 * result[1]:.2f}%, '
-            f'FPR95: {100 * result[2]:.2f}%, Accuracy: {100 * result[3]:.2f}%, Test Score: {100 * result[4]:.2f}%'
-        )
-        logger.add_epoch_info(info)
-        print(info)
+        metric = evaluate_detect(model, dataset_ind, dataset_ood_te, criterion, eval_acc, args, device)
+        logger.log(epoch, loss, *metric)
 
+    print(f"\n{'Final Statistics':=^80}")
     metrics = logger.get_statistics()
 
     Recorder.insert_row(
@@ -136,7 +132,7 @@ def train():
         accuracy=metrics.accuracy,
         score=metrics.score
     )
-    print(f"\n====================End Time: {get_now_datetime()}====================")
+    print(f"\n{'End Time: ' + get_now_datetime():=^80}")
 
 
 if __name__ == '__main__':

src/model/data_utils.py
@@ -127,6 +127,11 @@ def get_measures(_pos, _neg, recall_level=0.95):
     examples = np.squeeze(np.vstack((pos, neg)))
     labels = np.zeros(len(examples), dtype=np.int32)
     labels[:len(pos)] = 1
+    # print("examples.mean()", examples.mean())
+    data_min = examples.min()
+    data_max = examples.max()
+    examples = (examples - data_min) / (data_max - data_min)
+    # print("scaled examples.mean()", examples.mean())
     auroc = roc_auc_score(labels, examples)
     accuracy = accuracy_score(labels, (examples >= 0.5).astype(int))
     aupr = average_precision_score(labels, examples)
@@ -214,8 +219,8 @@ def evaluate_detect(model, dataset_ind, dataset_ood, criterion, eval_func, args,
     else:
         with torch.no_grad():
             test_ood_score = model.detect(dataset_ood, dataset_ood.node_idx, device, args).cpu()
-    min_length = min(len(test_ind_score), len(test_ood_score))
 
+    min_length = min(len(test_ind_score), len(test_ood_score))
     test_ind_score = test_ind_score[:min_length]
     test_ood_score = test_ood_score[:min_length]
 

src/outlier/knn.py
@@ -72,7 +72,7 @@ def generate_outliers(
     # The number of nearest neighbors to return
     k = min(len(dataset), k)
 
-    print("==================== Begin Generate Outliers ====================")
+    print(f"\n{'Begin Generate Outliers':=^80}")
     print(f"Number of OOD samples to generate: {num_sample_points}")
     print(f"Number of ID samples defined as points near the boundary: {num_boundary}")
     print(f"Number of boundaries used to generate outliers: {num_pick}")
@@ -158,7 +158,8 @@ def generate_outliers(
     sample_labels = torch.zeros(num_sample_points, dtype=torch.long, device=device)
 
     print(f"Time taken to generate outliers {round(time.time() - begin_time, 2)}s")
-    print("===================== End Generate Outliers =====================")
+    print(f"{'End Generate Outliers':=^80}")
+
     return Outlier(
         sample_points=sample_points,
         sample_edges=sample_edges,